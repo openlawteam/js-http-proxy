861a378ac082e9793e6c93eed82834e04d0a2c6b
Felipe Forbeck <felipe.forbeck@gmail.com>
2020-12-09 16:59:18 -0300

    spin up http proxy

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const request = require("request");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.get("/fetch", (req, res) => {
+  console.log(`/fetch ${req.query.url}`);
+  request(
+    { url: req.query.url, followAllRedirects: true },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).send("error");
+      }
+      res.send(body);
+    }
+  );
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
